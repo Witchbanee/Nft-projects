8b5824755c32a7361cbfa4015a22aec5ecf0fee4
Pavel <55561686+Witchbanee@users.noreply.github.com>
2022-02-18 13:37:51 +0300

    Add files via upload
    Правки

---
+++ NFT Practics Project/js/script.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 30,
+});
+
+
+// модалка
+
+
+let send = document.querySelector(".form__down-button");
+let popup = document.querySelector(".modal_visual_hidden");
+let closes = document.querySelector(".modal__close-button");
+
+let animation = document.querySelector(".modal");
+
+send.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("show-modal");
+});
+
+
+closes.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("show-modal");
+});
+
+
+
+// ________________

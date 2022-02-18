06565ccf4b6d80ecb2129351bdbb7facc64c9a79
Pavel <55561686+Witchbanee@users.noreply.github.com>
2022-02-18 15:32:31 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,14 +10,19 @@ let send = document.querySelector(".form__down-button");
 let popup = document.querySelector(".modal_visual_hidden");
 let closes = document.querySelector(".modal__close-button");
 
+let animation = document.querySelector(".modal");
+
 send.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.add("show-modal");
 });
 
+
 closes.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("show-modal");
 });
+
+
 
 // ________________
